5860fa2e818157f59555f4ff5e2a0de2275b5a2d
tianxihua <lazitxh@gmail.com>
2026-08-23 21:15:09 +0800

    Update config.js

--- docs/src/config.js
+++ docs/src/config.js
@@ -1,3 +1,5 @@
 window.DELTA_ONLINE_CONFIG = {
   firebaseDatabaseUrl: "https://shejiyouxi-d0d85-default-rtdb.firebaseio.com",
+  photonAppId: "3197b10d-d00f-4f59-98bf-c65b4b96f358",
+  photonRegion: "asia",
 };
